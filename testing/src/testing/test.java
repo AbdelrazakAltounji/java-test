15b3c6a5c2ed85ca58f326aeb09a0724e83554d8
Abdelrazak ALTOUNJI <abdelrazak.altounji@ibm.com>
2022-04-07 12:30:25 +0200

    1st

---
+++ testing/src/testing/test.java
@@ -0,0 +1,71 @@
+package testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.junit.Test;
+import java.net.URL;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+
+import java.net.MalformedURLException;
+
+
+
+public class test {
+	protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
+	public static String url = "http://localhost:4444/wd/hub";
+	public static void main(String[] args) throws InterruptedException  {
+	
+		String Test_ID="16";
+	
+	System.setProperty("webdriver.gecko.driver","C:\\Abdelrazak\\geckodriver.exe");
+   // WebDriver driver = new FirefoxDriver();
+
+	
+	FirefoxOptions options = new FirefoxOptions();
+	try {
+	driver.set(new RemoteWebDriver(new URL(url), options));}catch (MalformedURLException e) {
+        e.printStackTrace();
+    }
+	
+	
+	WebDriver webdriver = driver.get();
+    webdriver.navigate().to("https://www.google.com");
+   //WebElement username = webdriver.findElement(By.name("google"));
+    //System.out.println(username);
+    	
+    webdriver.manage().window().maximize();
+   
+    System.out.println(Test_ID);
+	//driver.get("https://www.google.com/");
+    //String status_du_test="0";
+    //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    Thread.sleep(1000);
+    
+    //json_str = "{" + " \"data\" : { \"type\" : \"instances\", \"attributes\" : {" + "\"instance-id\":"+ Test_ID +",\"exit-code\": " + status_du_test +  ",\"automated-execution-output\": \"THIS IS MY OUTPUT\" }}}";
+    //System.out.println(json_str);
+    
+    //webdriver.quit();
+
+	
+    
+    //WebDriver driver = new FirefoxDriver();
+    
+   //driver.get("https:\\www.toptal.com");
+   
+   //By locator = By.id("Developper")
+	//WebElement Username = driver.findElement(locator);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+}
